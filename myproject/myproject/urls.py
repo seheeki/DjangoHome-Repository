e51660643b8e95be00740a522b7a25e0cc5acc9c
seheeki <sehee519@naver.com>
2023-05-31 23:01:40 +0900

    Add main App to Django Project

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views as main_views
 
 urlpatterns = [
+    path('', main_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
